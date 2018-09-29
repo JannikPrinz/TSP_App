242ab595fc0d2e1ab60f6cb5a76debb79dac9ad9
Jannik <jannik.prinz@gmail.com>
2018-09-29 19:22:50 +0200

    [Feature] Add VS project with example tabbed application.

---
+++ TSP_App/TSP_App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TSP_App.Models;
+
+namespace TSP_App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
