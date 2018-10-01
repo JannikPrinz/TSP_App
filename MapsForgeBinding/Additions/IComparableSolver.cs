a5151e54a8cb9d4e779a864a5937d296964802d2
Jannik <jannik.prinz@gmail.com>
2018-10-01 22:53:52 +0200

    [Feature] Add project to bind Mapsforge via a C# wrapper.

---
+++ MapsForgeBinding/Additions/IComparableSolver.cs
@@ -0,0 +1,45 @@
+﻿namespace Org.Mapsforge.Core.Mapelements
+{
+    public partial class SymbolContainer : global::Org.Mapsforge.Core.Mapelements.MapElementContainer
+    {
+        public override int CompareTo(Java.Lang.Object o)
+        {
+            return CompareTo((MapElementContainer)o);
+        }
+    }
+
+    public partial class WayTextContainer : global::Org.Mapsforge.Core.Mapelements.MapElementContainer
+    {
+        public override int CompareTo(Java.Lang.Object o)
+        {
+            return CompareTo((MapElementContainer)o);
+        }
+    }
+}
+
+namespace Org.Mapsforge.Core.Model
+{
+    public partial class LatLong : global::Java.Lang.Object, global::Java.Lang.IComparable
+    {
+        int global::Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
+        {
+            return CompareTo((LatLong)o);
+        }
+    }
+
+    public partial class Point : global::Java.Lang.Object, global::Java.IO.ISerializable, global::Java.Lang.IComparable
+    {
+        int global::Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
+        {
+            return CompareTo((Point)o);
+        }
+    }
+
+    public partial class Tag : global::Java.Lang.Object, global::Java.IO.ISerializable, global::Java.Lang.IComparable
+    {
+        int global::Java.Lang.IComparable.CompareTo(Java.Lang.Object o)
+        {
+            return CompareTo((Tag)o);
+        }
+    }
+}
