a356dcdfcb95a12d63dc16e5e4523407e4fd0d2d
Jannik <jannik.prinz@gmail.com>
2018-10-02 11:35:25 +0200

    [Feature] Solve build problems in generated wrappers for the Mapsforge library.

---
+++ MapsForgeBinding/Additions/MapAndroidSolver.cs
@@ -0,0 +1,20 @@
+﻿using Java.Lang;
+
+namespace Org.Mapsforge.Map.Android.Graphics
+{
+    public sealed partial class AndroidGraphicFactory : global::Java.Lang.Object, global::Org.Mapsforge.Core.Graphics.IGraphicFactory
+    {
+        global::Org.Mapsforge.Core.Graphics.IHillshadingBitmap global::Org.Mapsforge.Core.Graphics.IGraphicFactory.CreateMonoBitmap(int width, int height, byte[] buffer, int padding, global::Org.Mapsforge.Core.Model.BoundingBox area)
+        {
+            return CreateMonoBitmap(width, height, buffer, padding, area);
+        }
+    }
+
+    public partial class AndroidPointTextContainer : global::Org.Mapsforge.Core.Mapelements.PointTextContainer
+    {
+        public override int CompareTo(Object o)
+        {
+            return CompareTo((Core.Mapelements.MapElementContainer)o);
+        }
+    }
+}
---
+++ MapsForgeBinding/Additions/MapSolver.cs
@@ -0,0 +1,10 @@
+﻿namespace Org.Mapsforge.Map.Layer.Hills
+{
+    public partial class MemoryCachingHgtReaderTileSource : global::Java.Lang.Object, global::Org.Mapsforge.Map.Layer.Hills.IShadeTileSource
+    {
+        void IShadeTileSource.SetDemFolder(Java.IO.File p0)
+        {
+            DemFolder = p0;
+        }
+    }
+}
